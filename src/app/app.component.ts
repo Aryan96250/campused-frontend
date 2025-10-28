cdbff8b0a99241140b3fdc5052c57a7129ca9e7b
aryan <aryan>
2025-10-28 11:16:21 +0530

    fixed new changes uninstall reanue cat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RevenueCatService } from './helpers/services/revenuecat.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +9,7 @@ import { RevenueCatService } from './helpers/services/revenuecat.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent { 
-  constructor(private revanueCat:RevenueCatService){
-   this.revanueCat.initialize();
+  constructor(){
+
   }
 }
